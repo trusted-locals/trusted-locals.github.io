db4bb23e530f71524d0c37015542aa4a5bb783db
Oliver Micke <olivermicke@outlook.com>
2020-03-22 20:04:09 +0100

    Updates

---
+++ precache-manifest.1435268f1664780646875a17238918c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5774df54fb63f220b6f8ee26b45a983c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8d18947f31894bcb960",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "8a3f037d85dcacba038d",
+    "url": "/static/js/2.2fb2dea9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2fb2dea9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8d18947f31894bcb960",
+    "url": "/static/js/main.814af81f.chunk.js"
+  },
+  {
+    "revision": "c29e51324190100c992b",
+    "url": "/static/js/runtime-main.df8b7b1f.js"
+  }
+]);
